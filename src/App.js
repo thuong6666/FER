fbbfb317b15e8e78429cb603f881db1dd8cc043d
thuonggg <96299762+thuong6666@users.noreply.github.com>
2023-10-09 14:53:43 +0700

    Update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Content9 from './components/content9';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      <Content9/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/content9.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './content9.css'
+import IMG1 from '../assets/callus.png'
+import IMG2 from '../assets/hackergirl.png'
+import GIF from '../assets/gif1.gif'
+export default function Content9() {
+  const callus = IMG1;
+  const hackergirl = IMG2;
+  const title = "You don’t have to see the whole staircase just take the first step";
+  const script = "Amet in a suspendisse convallis eget, Amet in a suspendisse convallis egetAmet in a";
+  const button1 = "Get Started";
+  const button2 = "Place a call";
+  const gif = GIF;
+  return (
+    <div className='content9'>
+      <div className=' row-fluid front'>
+        <div className='col-md-6 conimg'>
+        <img src={callus} alt='' className='callusimg' />
+        </div>
+        <div className='col-md-6 context'>
+          <div className='text'>
+            <span className='title'>{title}</span> <br></br>
+            <span className='textt' > {script} </span>
+          </div>
+          <div className='button'>
+            <button className='button1'>{button1}</button>
+            <button className='button2'>{button2}</button>
+          </div>
+        </div>
+      </div>
+
+      <div className='con'>
+        <div className='cover'>
+        </div>
+        <div className='gif row'>
+          <img src={gif} alt='' className='img1 col-md-4' />
+          <img src={gif} alt='' className='img2 col-md-4' />
+          <img src={gif} alt='' className='col-md-4' />
+        </div>
+        <img src={hackergirl} alt='' className='bgr' />
+        
+      </div>
+
+
+    </div>
+  )
+}
+
